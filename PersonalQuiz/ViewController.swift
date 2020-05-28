e4afa7a7f114a4fdf22821f0312b01cdb97f5448
Vladchere <vladchere.ok@mail.ru>
2020-05-29 00:20:19 +0700

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Vladislav on 29.05.2020.
+//  Copyright © 2020 Vladislav Cheremisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
